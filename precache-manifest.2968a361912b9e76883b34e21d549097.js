6f0ba87b3e40c7481aeca2631d701001c7add61c
Griffin McLennan <mclennangriffin@gmail.com>
2019-10-31 03:11:16 -0500

    Updates

---
+++ precache-manifest.2968a361912b9e76883b34e21d549097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b402fae292d0188f10ac1b4fbb3f944d",
+    "url": "/TSP_React/index.html"
+  },
+  {
+    "revision": "09b0eb272d7ef6d50d6c",
+    "url": "/TSP_React/static/css/main.ee455b6c.chunk.css"
+  },
+  {
+    "revision": "c26b23df0ac788cbfe1a",
+    "url": "/TSP_React/static/js/2.715ec4a4.chunk.js"
+  },
+  {
+    "revision": "09b0eb272d7ef6d50d6c",
+    "url": "/TSP_React/static/js/main.adcafd3f.chunk.js"
+  },
+  {
+    "revision": "f4dbe59b42fe968e489d",
+    "url": "/TSP_React/static/js/runtime-main.892b7acd.js"
+  }
+]);
